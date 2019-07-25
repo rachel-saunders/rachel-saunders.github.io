7454b7f4107f4a22c7d67048e36416940a684ae2
rachel-saunders <52934784+rachel-saunders@users.noreply.github.com>
2019-07-25 15:06:59 -0700

    Add files via upload

---
+++ mappages/scripts/supplychain.js
@@ -0,0 +1,521 @@
+var supplychain = {
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower France",
+        "place_name": "12 Allée Du Levant, 69890 La Tour-de-Salvagny, France"
+      },
+      "geometry": {
+        "coordinates": [
+          4.726069,
+          45.816077
+        ],
+        "type": "Point"
+      },
+      "id": "address.2030439743"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "NorSun AS",
+        "place_name": "Tangevegen 3, 6885 Årdalstangen, Norway",
+        "produce": "ignots and wafers"
+      },
+      "geometry": {
+        "coordinates": [
+          7.706991,
+          61.236838
+        ],
+        "type": "Point"
+      },
+      "id": "address.2934907184216448"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "Wacker Chemie AG, Nunchritz",
+        "place_name": "Friedrich-Von-Heyden-Platz 1, 01612 Nünchritz, Germany",
+        "produce": "polycrystalline silicon"
+      },
+      "geometry": {
+        "coordinates": [
+          13.39515,
+          51.298002
+        ],
+        "type": "Point"
+      },
+      "id": "address.4810182232596042"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "Hemlock Corp.",
+        "place_name": "12334 Geddes Road, Hemlock, Michigan 48626, United States",
+        "produce": "Polysilicon"
+      },
+      "geometry": {
+        "coordinates": [
+          -84.160654,
+          43.436264
+        ],
+        "type": "Point"
+      },
+      "id": "address.5156507756652178"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "Wacker Polysilicon",
+        "place_name": "Mcbryant Road Northwest, Charleston, Tennessee 37310, United States",
+        "produce": "polycrystalline silicon"
+      },
+      "geometry": {
+        "coordinates": [
+          -84.796076,
+          35.283907
+        ],
+        "type": "Point"
+      },
+      "id": "address.6850449806105114"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "REC Silicon ASA",
+        "place_name": "Fornebuveien 84, 1366 Lysaker, Norway",
+        "produce": "monocrystalline ignots"
+      },
+      "geometry": {
+        "coordinates": [
+          10.631623,
+          59.90261
+        ],
+        "type": "Point"
+      },
+      "id": "address.7982220330399400"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "REC Silicon",
+        "place_name": "3322 Road N Northeast, Moses Lake, Washington 98837, United States",
+        "produce": "monocrystalline ignots"
+      },
+      "geometry": {
+        "coordinates": [
+          -119.198713,
+          47.134918
+        ],
+        "type": "Point"
+      },
+      "id": "address.8235302069873390"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "REC Silicon",
+        "place_name": "119140 Rick Jones Way, Butte, Montana 59750, United States",
+        "produce": "monocrystalline ignots"
+      },
+      "geometry": {
+        "coordinates": [
+          -112.688913,
+          45.970868
+        ],
+        "type": "Point"
+      },
+      "id": "address.843572393726220"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "quartz",
+        "exports": "quartz",
+        "place_name": "Brazil",
+        "rank": 3,
+        "percent": 8.9
+      },
+      "geometry": {
+        "coordinates": [
+          -53,
+          -11
+        ],
+        "type": "Point"
+      },
+      "id": "country.12447454793682710"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "quartz",
+        "exports": "quartz",
+        "place_name": "India",
+        "rank": 2,
+        "percent": 15
+      },
+      "geometry": {
+        "coordinates": [
+          77,
+          21
+        ],
+        "type": "Point"
+      },
+      "id": "country.16892466316579250"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "quartz",
+        "exports": "quartz",
+        "place_name": "China",
+        "rank": 1,
+        "percent": 16
+      },
+      "geometry": {
+        "coordinates": [
+          103,
+          35
+        ],
+        "type": "Point"
+      },
+      "id": "country.17713928879157440"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "copper ore",
+        "exports": "copper ore",
+        "place_name": "Indonesia",
+        "rank": 4,
+        "percent": 5.4
+      },
+      "geometry": {
+        "coordinates": [
+          118,
+          -2
+        ],
+        "type": "Point"
+      },
+      "id": "country.2945973043114360"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "copper ore",
+        "exports": "copper ore",
+        "place_name": "Australia",
+        "rank": 3,
+        "percent": 7.2
+      },
+      "geometry": {
+        "coordinates": [
+          135,
+          -25
+        ],
+        "type": "Point"
+      },
+      "id": "country.3050683007346070"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "copper ore",
+        "exports": "copper ore",
+        "place_name": "Canada",
+        "rank": 5,
+        "percent": 4.8
+      },
+      "geometry": {
+        "coordinates": [
+          -109,
+          56
+        ],
+        "type": "Point"
+      },
+      "id": "country.4282270149587150"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "quartz",
+        "exports": "quartz",
+        "place_name": "Turkey",
+        "rank": 4,
+        "percent": 8.6
+      },
+      "geometry": {
+        "coordinates": [
+          36,
+          39
+        ],
+        "type": "Point"
+      },
+      "id": "country.4833608844176230"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "copper ore",
+        "exports": "copper ore",
+        "place_name": "Chile",
+        "rank": 1,
+        "percent": 28
+      },
+      "geometry": {
+        "coordinates": [
+          -71,
+          -31
+        ],
+        "type": "Point"
+      },
+      "id": "country.5834877801731110"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "copper ore",
+        "exports": "copper ore",
+        "place_name": "Peru",
+        "rank": 2,
+        "percent": 21
+      },
+      "geometry": {
+        "coordinates": [
+          -78,
+          -8
+        ],
+        "type": "Point"
+      },
+      "id": "country.8104362620964510"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "mine": "quartz",
+        "exports": "quartz",
+        "place_name": "Spain",
+        "rank": 5,
+        "percent": 8.4
+      },
+      "geometry": {
+        "coordinates": [
+          -3,
+          40
+        ],
+        "type": "Point"
+      },
+      "id": "country.8849824479570100"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place_name": "Philippines",
+        "manu_site": "SunPower Fab 4",
+        "CO2_scope1": 46
+      },
+      "geometry": {
+        "coordinates": [
+          121.066273,
+          14.27618
+        ],
+        "type": "Point"
+      },
+      "id": "fc202b0f918e3629ede1df02cdd2a61d"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place_name": "Santa Anastacia, Philippines",
+        "manu_site": "SunPower Fab 2",
+        "CO2_scope1": 1006,
+        "status": "closed"
+      },
+      "geometry": {
+        "coordinates": [
+          121.135141,
+          14.137501
+        ],
+        "type": "Point"
+      },
+      "id": "locality.16748148176263530"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Brighton",
+        "place_name": "Brighton, Victoria, Australia",
+        "CO2_scope1": 1
+      },
+      "geometry": {
+        "coordinates": [
+          144.999517,
+          -37.905482
+        ],
+        "type": "Point"
+      },
+      "id": "locality.7140506159894360"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "OCI Plant",
+        "place_name": "Soryong-dong, Gunsan-si, Jeollabuk-do, South Korea",
+        "produce": "polycrystalline silicon"
+      },
+      "geometry": {
+        "coordinates": [
+          126.662835,
+          35.979712
+        ],
+        "type": "Point"
+      },
+      "id": "neighborhood.4500559014707220"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Richmond",
+        "place_name": "Richmond, California, United States",
+        "CO2_scope1": 993
+      },
+      "geometry": {
+        "coordinates": [
+          -122.358354,
+          37.910501
+        ],
+        "type": "Point"
+      },
+      "id": "place.10112936257867690"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower San Jose",
+        "place_name": "San Jose, California, United States",
+        "CO2_scope1": 993
+      },
+      "geometry": {
+        "coordinates": [
+          -121.946716,
+          37.404787
+        ],
+        "type": "Point"
+      },
+      "id": "place.7704339974165690"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Cape Town",
+        "place_name": "Cape Town, Western Cape, South Africa",
+        "CO2_scope1": 5
+      },
+      "geometry": {
+        "coordinates": [
+          18.576651,
+          -33.968293
+        ],
+        "type": "Point"
+      },
+      "id": "place.8000320377767990"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "product_facility": "OCI Malaysia",
+        "place_name": "Bintulu, Sarawak, Malaysia",
+        "produce": "polycrystalline silicon"
+      },
+      "geometry": {
+        "coordinates": [
+          113.305171,
+          3.544446
+        ],
+        "type": "Point"
+      },
+      "id": "place.8749890402326853"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower TEMASOL",
+        "place_name": "Rabat, Rabat - Salé - Zemmour - Zaer, Morocco",
+        "CO2_scope1": 291
+      },
+      "geometry": {
+        "coordinates": [
+          -6.846631,
+          33.998192
+        ],
+        "type": "Point"
+      },
+      "id": "place.8898934748696270"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Modco",
+        "place_name": "Biñan, Philippines",
+        "CO2_scope1": 159,
+        "status": "closed"
+      },
+      "geometry": {
+        "coordinates": [
+          121.065836,
+          14.27815
+        ],
+        "type": "Point"
+      },
+      "id": "place.8969748587290700"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "install_site": "Solar Star",
+        "place_name": "Rosamond, California, United States"
+      },
+      "geometry": {
+        "coordinates": [
+          -118.333979,
+          34.929592
+        ],
+        "type": "Point"
+      },
+      "id": "place.9229282492996210"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Mexicali",
+        "place_name": "Mexicali, Baja California, Mexico",
+        "CO2_scope1": 42
+      },
+      "geometry": {
+        "coordinates": [
+          -115.532608,
+          32.622782
+        ],
+        "type": "Point"
+      },
+      "id": "place.9382283721312120"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "manu_site": "SunPower Malaysia",
+        "place_name": "Malacca, Malaysia",
+        "CO2_scope1": 82
+      },
+      "geometry": {
+        "coordinates": [
+          102.21123,
+          2.34049
+        ],
+        "type": "Point"
+      },
+      "id": "region.6885995336135960"
+    }
+  ],
+  "type": "FeatureCollection"
+}
